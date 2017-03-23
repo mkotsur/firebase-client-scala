8ba24f5f1faa6b40ce732b67164fa89a09dc3dc8
Mike Kotsur <miccots@gmail.com>
2017-03-23 12:55:32 +0200

    Implemented basic ops

--- src/main/scala/io/github/mkotsur/firebase/rest/FirebaseClient.scala
+++ src/main/scala/io/github/mkotsur/firebase/rest/FirebaseClient.scala
@@ -6,6 +6,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import io.circe.parser.decode
 import io.circe.{Decoder, Encoder}
 import io.github.mkotsur.firebase.auth.{AccessToken, AdminCredentials}
+import io.github.mkotsur.firebase.rest.FirebaseClient.FirebaseClientException
 
 import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext.Implicits._
@@ -14,6 +15,8 @@ import scala.util.Try
 import scalaj.http.Http
 
 object FirebaseClient {
+
+  class FirebaseClientException(message: String = "", cause: Throwable = null) extends RuntimeException(message, cause)
 
   def getToken(adminCredentials: AdminCredentials): Try[AccessToken] = Try {
     val googleCred = GoogleCredential.fromStream(new ByteArrayInputStream(adminCredentials.serviceAccount))
@@ -24,6 +27,83 @@ object FirebaseClient {
     scoped.refreshToken()
 
     AccessToken(scoped.getAccessToken)
+  }
+
+}
+
+class FirebaseClient(val projectId: String) {
+
+  private val baseUrl = s"https://$projectId.firebaseio.com"
+
+  def get[T](path: String)(implicit decoder: Decoder[T], token: AccessToken): Future[Option[T]] = Future {
+    Http(s"$baseUrl/$path.json").param("access_token", token.value).asString
+  } flatMap { response =>
+    decode[Option[T]](response.body) match {
+      case Right(v) => Future.successful(v)
+      case Left(e) => Future.failed(e)
+    }
+  }
+
+  def put[T](data: T, path: String)(implicit token: AccessToken, encoder: Encoder[T], decoder: Decoder[T]): Future[Option[T]] = Future {
+    Http(s"$baseUrl/$path.json")
+      .param("access_token", token.value)
+      .put(encoder(data).toString())
+      .header("content-type", "application/json")
+      .asString
+  } flatMap {
+    response => decode[Option[T]](response.body) match {
+      case Right(v) => Future.successful(v)
+      case Left(e) => Future.failed(e)
+    }
+  }
+
+  /**
+    * Pushes data and returns the child name of the new data.
+    */
+  def post[T](data: T, path: String)(implicit token: AccessToken, encoder: Encoder[T]): Future[String] = Future {
+    Http(s"$baseUrl/$path.json")
+      .param("access_token", token.value)
+      .postData(encoder(data).toString())
+      .header("content-type", "application/json")
+      .asString
+  } flatMap {
+    response => decode[Map[String, String]](response.body) match {
+      case Right(v) => Future.successful(v("name"))
+      case Left(e) => Future.failed(e)
+    }
+  }
+
+  /**
+    * Updates data and returns the child name of the new data.
+    */
+  def patch[T](data: T, path: String)(implicit token: AccessToken, encoder: Encoder[T], decoder: Decoder[T]): Future[T] = Future {
+    Http(s"$baseUrl/$path.json")
+      .param("access_token", token.value)
+      .postData(encoder(data).toString())
+      .method("PATCH") // should go *after* postData
+      .header("content-type", "application/json")
+      .asString
+  } flatMap {
+    response =>
+      decode[T](response.body) match {
+        case Right(v) => Future.successful(v)
+        case Left(e) => Future.failed(new FirebaseClientException(s"Could not decode response ${response.body}", e))
+      }
+  }
+
+  /**
+    * Deletes the data
+    */
+  def delete(path: String)(implicit token: AccessToken): Future[Unit] = Future {
+    Http(s"$baseUrl/$path.json")
+      .method("DELETE")
+      .param("access_token", token.value)
+      .asString
+  } flatMap {
+    response => response.body match {
+      case "null" => Future.successful()
+      case body => Future.failed(new FirebaseClientException(s"Expected response [null], but got [$body]"))
+    }
   }
 
 }
--- src/test/scala/io/github/mkotsur/firebase/rest/FirebaseClientTest.scala
+++ src/test/scala/io/github/mkotsur/firebase/rest/FirebaseClientTest.scala
@@ -4,13 +4,23 @@ import java.nio.file.{Files, Paths}
 
 import com.typesafe.config.ConfigFactory
 import configs.syntax._
+import io.circe.generic.auto._
 import io.github.mkotsur.firebase.auth.AdminCredentials
-import org.scalatest.{FunSpec, Matchers}
 import org.scalatest.TryValues._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{FunSpec, Matchers}
 
-class FirebaseClientTest extends FunSpec with Matchers {
+import scala.concurrent.duration.Duration
+
+class FirebaseClientTest extends FunSpec with Matchers with ScalaFutures {
 
   private val config = ConfigFactory.load("application-test.conf")
+
+  implicit val defaultPatience = PatienceConfig(
+    config.get[Duration]("patience.timeout").value,
+    config.get[Duration]("patience.interval").value
+  )
+
   private val projectId = config.get[String]("firebase.projectId").value
 
   private val validJsonKey = Files.readAllBytes(
@@ -23,16 +33,96 @@ class FirebaseClientTest extends FunSpec with Matchers {
 
   describe("Firebase client") {
 
-    it("should fetch access token based on service account private key") {
-      val adminCredential = AdminCredentials(validJsonKey)
-      val token = FirebaseClient.getToken(adminCredential)
-      token.success.value.value should not be empty
+    describe("token") {
+      it("should fetch access token based on service account private key") {
+        val adminCredential = AdminCredentials(validJsonKey)
+        val token = FirebaseClient.getToken(adminCredential)
+        token.success.value.value should not be empty
+      }
+
+      it("should return an error when trying to fetch access token if service account is incorrect") {
+        val adminCredential = AdminCredentials(inValidJsonKey)
+        val token = FirebaseClient.getToken(adminCredential)
+        token should not be null
+      }
     }
 
-    it("should return an error when trying to fetch access token if service account is incorrect") {
-      val adminCredential = AdminCredentials(inValidJsonKey)
-      val token = FirebaseClient.getToken(adminCredential)
-      token should not be null
+    describe("read") {
+      it("should read primitives from Firebase") {
+        val adminCredential = AdminCredentials(validJsonKey)
+        implicit val token = FirebaseClient.getToken(adminCredential).get
+        val fc = new FirebaseClient(projectId)
+        fc.get[Int]("eternal/shouldReadThis/age").futureValue shouldBe Some(100)
+      }
+
+      it("should read objects from Firebase") {
+        case class MyUser(name: String, age: Int)
+
+        val adminCredential = AdminCredentials(validJsonKey)
+        implicit val token = FirebaseClient.getToken(adminCredential).get
+        val fc = new FirebaseClient(projectId)
+        fc.get[MyUser]("eternal/shouldReadThis").futureValue shouldBe Some(MyUser("Tom", 100))
+      }
+
+      it("should return None if requested path does not exist") {
+        case class MyUser(name: String, age: Int)
+
+        val adminCredential = AdminCredentials(validJsonKey)
+        implicit val token = FirebaseClient.getToken(adminCredential).get
+        val fc = new FirebaseClient(projectId)
+        fc.get[MyUser]("eternal/doesNotExist").futureValue shouldBe None
+      }
+
+    }
+
+    describe("create and remove") {
+      it("should create primitives in Firebase") {
+        val adminCredential = AdminCredentials(validJsonKey)
+        implicit val token = FirebaseClient.getToken(adminCredential).get
+        val fc = new FirebaseClient(projectId)
+        fc.put(42, "temp/answer").futureValue shouldBe Some(42)
+        fc.get[Int]("temp/answer").futureValue shouldBe Some(42)
+        fc.delete("temp").futureValue shouldBe ()
+        fc.get[Int]("temp/answer").futureValue shouldBe None
+      }
+
+      it("should create objects in Firebase") {
+        case class MyCow(name: String)
+
+        val adminCredential = AdminCredentials(validJsonKey)
+        implicit val token = FirebaseClient.getToken(adminCredential).get
+        val fc = new FirebaseClient(projectId)
+        fc.put(MyCow("Henrietta"), "temp/cow").futureValue shouldBe Some(MyCow("Henrietta"))
+        fc.get[MyCow]("temp/cow").futureValue shouldBe Some(MyCow("Henrietta"))
+
+        fc.delete("temp").futureValue shouldBe ()
+        fc.get[MyCow]("temp/cow").futureValue shouldBe None
+      }
+    }
+
+    describe("update") {
+      it("should update values in Firebase") {
+        val adminCredential = AdminCredentials(validJsonKey)
+        implicit val token = FirebaseClient.getToken(adminCredential).get
+        val fc = new FirebaseClient(projectId)
+        fc.put("First", "temp/something").futureValue shouldBe Some("First")
+        fc.get[String]("temp/something").futureValue shouldBe Some("First")
+
+        fc.patch[Map[String, Int]](Map("something" -> 43), "temp/").futureValue shouldBe Map("something" -> 43)
+        fc.get[Int]("temp/something").futureValue shouldBe Some(43)
+        fc.delete("temp").futureValue shouldBe ()
+      }
+
+      it("should push values into Firebase") {
+        val adminCredential = AdminCredentials(validJsonKey)
+        implicit val token = FirebaseClient.getToken(adminCredential).get
+        val fc = new FirebaseClient(projectId)
+        val pushedChildName = fc.post[Int](43, "temp/pushed").futureValue
+        pushedChildName should not be empty
+
+        fc.get[Int](s"temp/pushed/$pushedChildName").futureValue shouldBe Some(43)
+        fc.delete("temp").futureValue shouldBe ()
+      }
     }
   }
 
